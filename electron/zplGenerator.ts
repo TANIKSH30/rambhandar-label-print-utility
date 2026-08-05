05e6763b99789bffed54269f48d37700cd212d89
TANIKSH30 <tanikshnimje@gmail.com>
2026-08-05 18:27:30 +0530

    fix: position barcode cleanly after BEST BEFORE line (FO485,53 and FT565,298)

--- electron/zplGenerator.ts
+++ electron/zplGenerator.ts
@@ -56,9 +56,9 @@ export function generateZPL(data: LabelData, copies: number = 1): string {
     `^A0B,34,24^FDPACKED DATE: ${data.packedDate}^FS`,
     "^FT441,368",
     `^A0B,34,24^FDBEST BEFORE : ${data.bestBefore}^FS`,
-    "^FO480,53",
+    "^FO485,53",
     `^BY4^BCB,62,N,N^FD>;1${data.barcodeNumber}^FS`,
-    "^FT565,248",
+    "^FT565,298",
     `^A0B,34,46^FD${data.barcodeNumber}^FS`,
     `^PQ${copies},0,1,Y`,
     "^XZ"
